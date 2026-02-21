66090e40f261819125895083e92811cfeb003ebd
nqmagwaz021 <nqmagwaz021@student.wethinkcode.co.za>
2026-02-21 08:11:41 +0200

    Add PostgreSQL dependencies

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,18 @@ from pathlib import Path
 import os
 import dj_database_url
 
+import os
+from django.contrib.auth import get_user_model
+
+if os.environ.get("CREATE_SUPERUSER") == "True":
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+    if username and email and password:
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
